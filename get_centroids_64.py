ef7ed2eddc40881ee5af0e2c36c36fd29f5e7bac
Ian Pan <gotkoffee@gmail.com>
2017-02-24 17:03:31 -0500

    update get_centroids_64.py so that train/test splits are assigned after (not before) available keys are considered

--- get_centroids_64.py
+++ get_centroids_64.py
@@ -2,7 +2,7 @@ import ml_stuff
 import pandas as pd 
 import subprocess
 
-labels_file = "/users/ipan/scratch/ml_code/ProTECTIII_Labels_v2.csv")
+labels_file = "/users/ipan/scratch/ml_code/ProTECTIII_Labels_v2.csv"
 train_test_split_out = "/users/ipan/scratch/ml_code/neg_tilt/train_test_split.csv"
 keys_dir = "/users/ipan/scratch/ml_code/neg_tilt/keys/"
 pkl_file = "/users/ipan/scratch/ml_code/neg_tilt/km-pickles/mbkm_64_1.pkl"
@@ -11,15 +11,16 @@ centroids_csv_file = "/users/ipan/scratch/ml_code/neg_tilt/km-centroids/mbkm_64_
 labels = pd.read_csv(labels_file)
 labels = labels[(labels.exclude == False)]
 
-train_test_df = ml_stuff.assign_train_test(labels.id, labels.pos_OR, train_test_split_out) 
-
 all_keys = subprocess.check_output("ls " + keys_dir, shell=True)
 all_keys = all_keys.split("\n")
 all_keys = [i if i != '' else None for i in all_keys]
 all_keys = filter(None, all_keys)
-
 all_keys_df = pd.DataFrame({'file': all_keys,
                             'name': [int(i[0:len(i)-4]) for i in all_keys]})
+
+labels = labels.merge(all_keys_df,left_on"id",right_on="name")
+
+train_test_df = ml_stuff.assign_train_test(labels.id, labels.pos_OR, train_test_split_out) 
 
 train_df = train_test_df[(train_test_df.set == "train")]
 train_keys = all_keys_df.merge(train_df,left_on="name",right_on="id")
